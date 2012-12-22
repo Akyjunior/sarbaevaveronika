ac35ecc595b014091ca1dd047d0541bed4be9562
Deyarov Ruslan <sm00th1980@mail.ru>
2012-12-22 14:26:52 +0400

    добавил проверку

--- inv_mod.py
+++ inv_mod.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-n = 97
-m = 30
+n = n_orig = 97
+m = m_orig = 30
 
 A=[1,0]
 B=[0,1]
@@ -26,9 +26,17 @@ while n > 1:
 	A.append(_a)
 	B.append(_b)
 
-	print "n=%s, m=%s, Ri=%s, Qi=%s, ai=%s, bi=%s" % (n,m, _r, _q, _a, _b)
+	print "n=%s, m=%s, Ri=%s, Qi=%s, ai=%s, bi=%s" % (n, m, _r, _q, _a, _b)
 	n = m
 	m = _r
 	i = i + 1
 
-print "1/detA = %s " % B[i - 1]
+result = B[i - 1]
+
+if result < 0:
+	result = result + n_orig
+
+print "Ответ: 1/detA = %s " % result
+
+#проверяем результат
+print "Проверяем результат: (%s*%s)mod(%s) => %s " % (m_orig, result, n_orig, (m_orig*result) % n_orig)
